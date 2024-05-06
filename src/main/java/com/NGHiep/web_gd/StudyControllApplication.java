6f14513d6cc2bd975b60177c8f93aee96b7578bf
NGHiep <nghiep010804@gmail.com>
2024-05-07 00:05:22 +0700

    create project

---
+++ src/main/java/com/NGHiep/web_gd/StudyControllApplication.java
@@ -0,0 +1,13 @@
+package com.NGHiep.web_gd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyControllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyControllApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/NGHiep/web_gd/StudyControllApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NGHiep.web_gd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyControllApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
